8069b1b46f2696c562ada585570e9005595d16e7
Keshara Madushanka <122123096+KesharaMadushanka@users.noreply.github.com>
2023-06-08 18:10:43 +0530

    Backend Project Initialization

---
+++ src/main/java/com/example/carrentingsystem/CarRentingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrentingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentingSystemApplication.class, args);
+    }
+
+}
